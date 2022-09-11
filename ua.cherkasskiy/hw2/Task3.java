91f87bdee342e6ed71c6e17a47e24eab46a698d9
PopayMan <super.popayman@gmail.com>
2022-09-11 20:36:50 +0200

    EvenOddChecker

---
+++ ua.cherkasskiy/hw2/Task3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = reader.nextInt();
+        evenOddChecker(num);
+    }
+
+    private static <num> void evenOddChecker(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + "even");
+        } else {
+            System.out.println(num + "odd");
+        }
+    }
+}
