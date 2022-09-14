a4b406d7776ab04af172f787677d7e82a1eaf3d7
PopayMan <super.popayman@gmail.com>
2022-09-14 09:15:23 +0200

    RebuildPalindrome

--- src/ua/cherkasskiy/hw3/Task2.java
+++ src/ua/cherkasskiy/hw3/Task2.java
@@ -3,24 +3,8 @@ package ua.cherkasskiy.hw3;
 import java.util.Scanner;
 
 public class Task2 {
-    public static String reverse(String word) {
-        StringBuilder reversed = new StringBuilder(word.length());
-        for (int i = word.length(); i > 0; i--) {
-            reversed.append(word.charAt(i - 1));
-        }
-        return word;
-    }
 
-
-    private static String removeSpace(String aword) {
-        StringBuilder buffer = new StringBuilder();
-        for (int i = 0; i < aword.length(); i++) {
-            if (aword.charAt(i) != ' ') {
-                buffer.append(aword.charAt(i));
-            }
-        }
-        return buffer.toString();
-    }
+    private static java.lang.String String;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -31,6 +15,23 @@ public class Task2 {
             System.out.println("The string " + word + " is a palindrome");
         } else {
             System.out.println("The string " + word + " is not a palindrome");
+            removeSpace(String, word);
         }
+    }
+    public static String reverse(String word) {
+        StringBuilder reversed = new StringBuilder(word.length());
+        for (int i = word.length(); i > 0; i--) {
+            reversed.append(word.charAt(i - 1));
+        }
+        return word;
+    }
+    private static String removeSpace(String word, java.lang.String s) {
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != ' ') {
+                buffer.append(word.charAt(i));
+            }
+        }
+        return buffer.toString();
     }
 }
