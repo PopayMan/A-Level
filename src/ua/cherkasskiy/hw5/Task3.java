d72b24d090bd3786185ddfde43afaba3e7652d38
PopayMan <super.popayman@gmail.com>
2022-09-26 19:41:33 +0200

    New

--- src/ua/cherkasskiy/hw5/Task3.java
+++ src/ua/cherkasskiy/hw5/Task3.java
@@ -6,13 +6,14 @@ public class Task3 {
         int[][] numbers = fillArray(8);
         System.out.println();
       changeValueArray(numbers);
+        System.out.println(Arrays.deepToString(numbers));
     }
     public static int[][] changeValueArray(int[][] numbers) {
         int[][] newNumbers = new int[numbers.length][numbers[1].length];
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
                 newNumbers[i][j] = numbers[j][i];
-                System.out.println(Arrays.deepToString(numbers));
+
             }
         }
         return newNumbers;
@@ -24,7 +25,6 @@ public class Task3 {
             int j;
             for (j = 0; j < numbers[i].length; j++) {
                 numbers[i][j] = random.nextInt(10);
-                System.out.println(Arrays.deepToString(numbers));
             }
         }
         return numbers;
